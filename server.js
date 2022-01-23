be1c89f18aa8c0d9609647459a552738ae1596b1
DanielWestiner <daniel.westiner@gmail.com>
2022-01-22 22:37:56 -0500

    added routing, set up paths

--- server.js
+++ server.js
@@ -2,29 +2,31 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs')
 
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+const api = require('./routes/api');
+const notes = require('./routes/notes')
 
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use('/', notes);
+app.use('/api', api);
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
-
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+

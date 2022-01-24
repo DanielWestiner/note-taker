b8c4b431da1bc33f5796a8ec0cd66324833048f9
DanielWestiner <daniel.westiner@gmail.com>
2022-01-23 19:21:58 -0500

    updated routing, creating new get request code

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const notes = require('../routes/notes.js')
+
+router.use(notes);
 
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -2,17 +2,23 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
+const savedNotes = require('../db/db.json')
+
+
+router.get('/notes', (req, res) => {
+    res.json(savedNotes);
+});
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../db/db.json"));
+  res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 router.post("/notes", (req, res) => {
-  let currentNote = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   let newNote = req.body;
   currentNote.push(newNote);
 
-  fs.writeFile("../db/db.json", JSON.stringify(currentNote), (err) => {
+  fs.writeFile("./db/db.json", JSON.stringify(currentNote), (err) => {
     if (err) throw err;
     console.log("Note has been saved.");
     res.json(currentNote);

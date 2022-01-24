5f13e6c48c40a4be915f8db348dc4a8369a8070f
DanielWestiner <daniel.westiner@gmail.com>
2022-01-23 23:26:50 -0500

    fixed notes.html appearing

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,51 @@ const router = express.Router();
 const path = require('path');
 const notes = require('../routes/notes.js')
 
+const savedNotes = require('../db/db.json')
+
 router.use(notes);
+
+router.get('/notes', (req, res) => {
+    res.json(savedNotes);
+});
+
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+router.post("/notes", (req, res) => {
+  let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  currentNote.push(newNote);
+  
+
+  fs.writeFile("./db/db.json", JSON.stringify(currentNote), (err) => {
+    if (err) throw err;
+    console.log("Note has been saved.");
+    res.json(currentNote);
+  });
+});
+
+
+
+router.post('/notes', (req, res) => {
+    const { title, text} = req.body;
+    const newNote = { title, text }
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const existingNotes = JSON.parse(data);
+      existingNotes.push(newNote);
+  
+    });
+  });
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
 
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -2,51 +2,15 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
-const savedNotes = require('../db/db.json')
 
 
-router.get('/notes', (req, res) => {
-    res.json(savedNotes);
-});
 
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./db/db.json"));
-});
 
-router.post("/notes", (req, res) => {
-  let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  let newNote = req.body;
-  currentNote.push(newNote);
-  
 
-  fs.writeFile("./db/db.json", JSON.stringify(currentNote), (err) => {
-    if (err) throw err;
-    console.log("Note has been saved.");
-    res.json(currentNote);
-  });
-});
-
-router.post('/notes', (req, res) => {
-    const { title, text} = req.body;
-    const newNote = { title, text }
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const existingNotes = JSON.parse(data);
-      existingNotes.push(newNote);
-  
-    });
-  });
   
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-})
 
 module.exports = router;

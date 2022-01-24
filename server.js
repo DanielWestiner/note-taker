691348363c2934c6e8e264f2f531f5c7608c4cd9
DanielWestiner <daniel.westiner@gmail.com>
2022-01-23 19:32:25 -0500

    added a app.post to server

--- server.js
+++ server.js
@@ -27,6 +27,20 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.post('/api/notes', (req, res) => {
+  const { title, text} = req.body;
+  const newNote = { title, text }
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const existingNotes = JSON.parse(data);
+    existingNotes.push(newNote);
+
+  });
+});
+
 
 // GET Route for anything else that user types in
 app.get('*', (req, res) =>

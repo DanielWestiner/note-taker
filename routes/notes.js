c59a6059ffe80a0f49543ba0d56210094ca9b990
DanielWestiner <daniel.westiner@gmail.com>
2022-01-23 19:38:17 -0500

    tried fixing linking the notes.html

--- routes/notes.js
+++ routes/notes.js
@@ -17,6 +17,7 @@ router.post("/notes", (req, res) => {
   let currentNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   let newNote = req.body;
   currentNote.push(newNote);
+  
 
   fs.writeFile("./db/db.json", JSON.stringify(currentNote), (err) => {
     if (err) throw err;
@@ -24,6 +25,21 @@ router.post("/notes", (req, res) => {
     res.json(currentNote);
   });
 });
+
+router.post('/notes', (req, res) => {
+    const { title, text} = req.body;
+    const newNote = { title, text }
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const existingNotes = JSON.parse(data);
+      existingNotes.push(newNote);
+  
+    });
+  });
+  
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
